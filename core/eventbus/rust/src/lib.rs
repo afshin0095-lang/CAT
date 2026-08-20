e7fc6fc9d55dbb16f0f8f19a333d06e89273c60a
afshin0095-lang <afshin0095@gmail.com>
2026-08-20 10:40:03 +0330

    feat(eventbus): expose durable contract layer

--- core/eventbus/rust/src/lib.rs
+++ core/eventbus/rust/src/lib.rs
@@ -2,12 +2,22 @@
 #![deny(clippy::all)]
 
 mod bus;
+mod durable;
 mod envelope;
 mod error;
+mod in_memory;
+mod ports;
+mod registry;
+mod reliability;
 
 pub use bus::{EventBus, EventHandler, PublishOutcome, SubscriptionId};
+pub use durable::{DeadLetter, DeadLetterStore, EventCodec, InMemoryDeadLetterStore, JsonEventCodec};
 pub use envelope::{EventEnvelope, EventKind};
 pub use error::{EventBusError, EventBusResult};
+pub use in_memory::{InMemoryIdempotency, InMemoryInbox, InMemoryOutbox};
+pub use ports::{EventTransport, IdempotencyStore, InboxStore, OutboxStore};
+pub use registry::{Compatibility, EventContract, EventRegistry};
+pub use reliability::{DeliveryRecord, DeliveryState, RetryPolicy};
 
 /// Trait implemented by typed events crossing a CAT event boundary.
 pub trait CatEvent: serde::Serialize + serde::de::DeserializeOwned + Send + Sync + 'static {
