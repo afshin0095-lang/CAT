18ea637937af3577f1215816cec59e4918acb27c
afshin0095-lang <afshin0095@gmail.com>
2026-08-23 19:29:07 +0330

    fix(planning): include planning contract tests

--- core/planning/rust/src/lib.rs
+++ core/planning/rust/src/lib.rs
@@ -1,6 +1,8 @@
 mod model;
 mod trace;
 mod validation;
+#[cfg(test)]
+mod tests;
 
 pub use model::{Plan, PlanBuilder, PlanId, PlanStatus, PlanStep, StepId, StepKind, StepDependency};
 pub use trace::{PlanTrace, PlanTraceEntry, PlanTraceKind};
