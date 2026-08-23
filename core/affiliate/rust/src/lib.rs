241a95931e151a254fcee984cdb94a5b37a055ac
afshin0095-lang <afshin0095@gmail.com>
2026-08-24 02:17:15 +0330

    feat(affiliate): expose domain lifecycle API

--- core/affiliate/rust/src/lib.rs
+++ core/affiliate/rust/src/lib.rs
@@ -1,11 +1,15 @@
+pub mod error;
 pub mod model;
+pub mod service;
 
+pub use error::{AffiliateDomainError, AffiliateDomainResult};
 pub use model::{
     Affiliate, AffiliateId, AffiliateKind, CommissionObligation, CommissionObligationId,
     Conversion, ConversionId, ConversionState, Merchant, MerchantId, Offer, OfferId, OfferStatus,
     Product, ProductId, Program, ProgramId, ProgramStatus, Referral, ReferralId, ReferralState,
     VersionedName,
 };
+pub use service::AffiliateDomain;
 
 pub const DOMAIN_NAME: &str = "affiliate";
 pub const DOMAIN_VERSION: u16 = 1;
