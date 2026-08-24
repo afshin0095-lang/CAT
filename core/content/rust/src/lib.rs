26e960f51118327481445dfa29c12b4bca843169
afshin0095-lang <afshin0095@gmail.com>
2026-08-25 03:23:57 +0330

    feat(content): export publication planning boundary

--- core/content/rust/src/lib.rs
+++ core/content/rust/src/lib.rs
@@ -5,6 +5,7 @@ pub mod error;
 pub mod events;
 pub mod memory_repository;
 pub mod model;
+pub mod publication;
 pub mod repository;
 pub mod service;
 
@@ -12,6 +13,7 @@ pub use error::{ContentDomainError, ContentDomainResult};
 pub use events::{ContentPublished, ContentVersionCreated};
 pub use memory_repository::InMemoryContentRepository;
 pub use model::{ContentId, ContentKind, ContentRecord, ContentStatus, ContentVersion};
+pub use publication::{PublicationPlan, PublicationRecord, PublicationState, PublicationTarget};
 pub use repository::ContentRepository;
 pub use service::ContentDomain;
 
