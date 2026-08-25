5e17d61d9fbe27c08a8fab768be82781977ea610
afshin0095-lang <afshin0095@gmail.com>
2026-08-25 04:11:33 +0330

    refactor(content): register revision lineage module

--- core/content/rust/src/lib.rs
+++ core/content/rust/src/lib.rs
@@ -7,6 +7,7 @@ pub mod memory_repository;
 pub mod model;
 pub mod repository;
 pub mod service;
+pub mod versioning;
 
 pub use error::{ContentDomainError, ContentDomainResult};
 pub use events::{ContentPublished, ContentVersionCreated};
@@ -14,6 +15,7 @@ pub use memory_repository::InMemoryContentRepository;
 pub use model::{ContentId, ContentKind, ContentRecord, ContentStatus, ContentVersion};
 pub use repository::ContentRepository;
 pub use service::ContentDomain;
+pub use versioning::{build_revision, RevisionPlan};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ContentEventMetadata {
