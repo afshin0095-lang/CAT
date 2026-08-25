575aabd9d425880be59440a550636c2b50d9566e
afshin0095-lang <afshin0095@gmail.com>
2026-08-25 04:11:20 +0330

    feat(content): version content creation event contract

--- core/content/rust/src/events.rs
+++ core/content/rust/src/events.rs
@@ -5,15 +5,23 @@ use uuid::Uuid;
 use crate::ContentId;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ContentVersionCreated { pub content_id: ContentId, pub version: u32 }
+pub struct ContentVersionCreated {
+    pub content_id: ContentId,
+    pub version: u32,
+    pub source_version: Option<u32>,
+}
 
 impl CatEvent for ContentVersionCreated {
     const TYPE: &'static str = "content.version.created";
-    const VERSION: u16 = 1;
+    const VERSION: u16 = 2;
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ContentPublished { pub content_id: ContentId, pub version: u32, pub publication_id: Uuid }
+pub struct ContentPublished {
+    pub content_id: ContentId,
+    pub version: u32,
+    pub publication_id: Uuid,
+}
 
 impl CatEvent for ContentPublished {
     const TYPE: &'static str = "content.published";
