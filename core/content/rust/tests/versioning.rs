ee0118e48ad054fc4ae2e0d41ca1ee63ac688a0a
afshin0095-lang <afshin0095@gmail.com>
2026-09-08 04:44:24 +0330

    fix(content): import repository trait for integration tests

--- core/content/rust/tests/versioning.rs
+++ core/content/rust/tests/versioning.rs
@@ -1,11 +1,10 @@
-use cat_content::{ContentDomain, ContentDomainError, ContentKind, ContentStatus, InMemoryContentRepository};
+use cat_content::{ContentDomain, ContentDomainError, ContentKind, ContentRepository, ContentStatus, InMemoryContentRepository};
 
 #[test]
 fn revision_creates_new_identity_and_preserves_source_version() {
     let mut repo = InMemoryContentRepository::default();
     let original = ContentDomain::create(&mut repo, ContentKind::Article, "v1", "body v1", 1).unwrap();
     let revised = ContentDomain::revise(&mut repo, original.id, "v2", "body v2", 2).unwrap();
-
     assert_ne!(original.id, revised.id);
     assert_eq!(original.version.version, 1);
     assert_eq!(original.version.source_version, None);
@@ -20,29 +19,15 @@ fn revision_creates_new_identity_and_preserves_source_version() {
 fn revision_rejects_non_monotonic_versions() {
     let mut repo = InMemoryContentRepository::default();
     let original = ContentDomain::create(&mut repo, ContentKind::Article, "v1", "body", 3).unwrap();
-
-    assert_eq!(
-        ContentDomain::revise(&mut repo, original.id, "bad", "body", 3),
-        Err(ContentDomainError::InvalidVersion)
-    );
-    assert_eq!(
-        ContentDomain::revise(&mut repo, original.id, "bad", "body", 2),
-        Err(ContentDomainError::InvalidVersion)
-    );
+    assert_eq!(ContentDomain::revise(&mut repo, original.id, "bad", "body", 3), Err(ContentDomainError::InvalidVersion));
+    assert_eq!(ContentDomain::revise(&mut repo, original.id, "bad", "body", 2), Err(ContentDomainError::InvalidVersion));
 }
 
 #[test]
 fn revision_rejects_empty_payloads_without_mutating_source() {
     let mut repo = InMemoryContentRepository::default();
     let original = ContentDomain::create(&mut repo, ContentKind::Article, "stable", "body", 1).unwrap();
-
-    assert_eq!(
-        ContentDomain::revise(&mut repo, original.id, " ", "next", 2),
-        Err(ContentDomainError::EmptyTitle)
-    );
-    assert_eq!(
-        ContentDomain::revise(&mut repo, original.id, "next", " ", 2),
-        Err(ContentDomainError::EmptyBody)
-    );
+    assert_eq!(ContentDomain::revise(&mut repo, original.id, " ", "next", 2), Err(ContentDomainError::EmptyTitle));
+    assert_eq!(ContentDomain::revise(&mut repo, original.id, "next", " ", 2), Err(ContentDomainError::EmptyBody));
     assert_eq!(repo.get(original.id).unwrap().version.version, 1);
 }
