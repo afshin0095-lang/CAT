316c75f5dce4509e2185a3a1c3b742bd6b053c84
afshin0095-lang <afshin0095@gmail.com>
2026-09-09 14:06:32 +0330

    feat(affiliate): export discovery source SPI

--- core/affiliate/rust/src/lib.rs
+++ core/affiliate/rust/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod attribution;
 pub mod commission_rules;
 pub mod discovery;
+pub mod discovery_source;
 pub mod error;
 pub mod events;
 pub mod memory_repository;
@@ -22,6 +23,7 @@ pub use service::AffiliateDomain;
 pub use attribution::{apply_model, click_within_window, Attribution, AttributionId, AttributionModel, AttributionResult, AttributionTouch, ConversionEvent, ConversionEventId, ConversionEventType};
 pub use commission_rules::{compute_commission, recurring_cap_exceeded, CommissionRule, CommissionRuleBuilder, CommissionSubRule, CommissionTrigger, CommissionType};
 pub use discovery::{canonical_key, DiscoveryCandidate, DiscoveryEngine, DiscoveryError, DiscoveryOpportunity, DiscoveryRequest, DiscoveryResult};
+pub use discovery_source::{DiscoveryIngestion, DiscoverySource, DiscoverySourceBatch, DiscoverySourceCapability, DiscoverySourceError, DiscoverySourceFuture, DiscoverySourceId, DiscoverySourceInfo, DiscoverySourceKind, DiscoverySourceRegistry, DiscoverySourceRequest};
 pub use network_adapter::{NetworkAdapter, NetworkConversion, NetworkId, NetworkInfo, NetworkProgram, NetworkRegistry};
 pub use scoring::{rank_programs, score_content_potential, score_earning_potential, ProgramScore, ProgramVerdict};
 pub use tracking::{build_utm_link, Click, ClickFraudFlag, ClickId, ExternalUserId, Identity, IdentityId, Link, LinkId, UtmParams, VelocityChecker};
