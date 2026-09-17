f61b3df3c5290ee537c4fbed889bb8600e506235
afshin0095-lang <afshin0095@gmail.com>
2026-09-18 03:06:30 +0330

    feat(prompt): add prompt policy boundary

---
+++ core/prompt/rust/src/policy.rs
@@ -0,0 +1,39 @@
+use std::collections::BTreeSet;
+use crate::{PromptDocument, PromptVariable};
+
+#[derive(Clone, Debug, Default)]
+pub struct PromptPolicy {
+    allowed_variables: BTreeSet<String>,
+    forbidden_sensitive_variables: bool,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PromptPolicyDecision {
+    pub allowed: bool,
+    pub blocked_variables: Vec<String>,
+}
+
+impl PromptPolicy {
+    pub fn new<I, S>(allowed_variables: I) -> Self
+    where I: IntoIterator<Item = S>, S: Into<String> {
+        Self {
+            allowed_variables: allowed_variables.into_iter().map(Into::into).collect(),
+            forbidden_sensitive_variables: true,
+        }
+    }
+    pub fn allow_sensitive_variables(mut self, allowed: bool) -> Self {
+        self.forbidden_sensitive_variables = !allowed;
+        self
+    }
+    pub fn evaluate(&self, document: &PromptDocument) -> PromptPolicyDecision {
+        let blocked_variables = document.variables.values()
+            .filter(|variable| !self.allowed_variables.contains(&variable.name)
+                || (self.forbidden_sensitive_variables && variable.sensitive))
+            .map(|variable| variable.name.clone()).collect();
+        PromptPolicyDecision { allowed: blocked_variables.is_empty(), blocked_variables }
+    }
+    pub fn permits(&self, variable: &PromptVariable) -> bool {
+        self.allowed_variables.contains(&variable.name)
+            && !(self.forbidden_sensitive_variables && variable.sensitive)
+    }
+}
