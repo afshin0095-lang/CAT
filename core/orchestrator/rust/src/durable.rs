25dc1c763728f25c1c05cfbe6e16fcccd193e723
afshin0095-lang <afshin0095@gmail.com>
2026-09-24 22:48:22 +0330

    test(orchestrator): declare durable test capability

--- core/orchestrator/rust/src/durable.rs
+++ core/orchestrator/rust/src/durable.rs
@@ -150,6 +150,7 @@ mod tests {
                 version: 1,
                 steps: vec![WorkflowStep {
                     id: "work".into(),
+                    capability_id: cat_kernel::CapabilityId::new("cat.capability.test.work.v1").unwrap(),
                     dependencies: vec![],
                     state: StepState::Ready,
                     attempt: 0,
